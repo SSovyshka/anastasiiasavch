9810125efd8bf7273b4eed14e0a7110712ec3f34
SSovyshka <91850335+SSovyshka@users.noreply.github.com>
2021-12-28 11:21:13 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            center: true,
+            items:2,
+            loop:false,
+            margin:30,
+            autoWidth:false,
+            startPosition:1,
+            nav:true,
+            navText : ["",""],
+            loop: true,
+        }
+        
+    );
+  });
